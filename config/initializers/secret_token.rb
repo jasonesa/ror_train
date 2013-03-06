0fb6cdd772a0818be97ff7a78b991f0213b99d31
jzumbado-rzf <jason.zumbado@boszdigital.com>
2013-03-06 08:48:51 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorTrain::Application.config.secret_token = 'dfe731dfa708c20cfe741fc500a3a6353886417865fee0da0f7daf782a8270a5783e2885781fc4eac32df20f8da6312b079a6d48acec0c845ff429a12b4b6b22'
